57b1c7859d35c9c8834180342eb101ec448907ad
nikopa <nikopa@ttu.ee>
2020-01-16 16:15:37 +0200

    Added new columns to database tables. New data added.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,32 +1,71 @@
-INSERT INTO doctor(name) VALUES ('Katie Cole');
-INSERT INTO doctor(name) VALUES ('Winifred Miles');
-INSERT INTO doctor(name) VALUES ('Julia Love');
-INSERT INTO doctor(name) VALUES ('Efa Lim');
-INSERT INTO doctor(name) VALUES ('Cydney Medrano');
-INSERT INTO doctor(name) VALUES ('Leandro Doherty');
-INSERT INTO doctor(name) VALUES ('Chantel Burns');
-INSERT INTO doctor(name) VALUES ('Keeva Ryan');
+INSERT INTO nikopa_dental_clinic.doctor(name, telephone, email) VALUES ('Katie Cole', '+372 776 8420', 'katie.cole@nikopa.ee');
+INSERT INTO nikopa_dental_clinic.doctor(name, telephone, email) VALUES ('Winifred Miles', '+372 598 0014', 'winifred.miles@nikopa.ee');
+INSERT INTO nikopa_dental_clinic.doctor(name, telephone, email) VALUES ('Julia Love', '+372 807 0992', 'julia.love@nikopa.ee');
+INSERT INTO nikopa_dental_clinic.doctor(name, telephone, email) VALUES ('Efa Lim', '+372 777 7645', 'efa.lim@nikopa.ee');
+INSERT INTO nikopa_dental_clinic.doctor(name, telephone, email) VALUES ('Cydney Medrano', '+372 864 0001', 'cydney.medrano@nikopa.ee');
+INSERT INTO nikopa_dental_clinic.doctor(name, telephone, email) VALUES ('Leandro Doherty', '+372 861 0171', 'leandro.doherty@nikopa.ee');
+INSERT INTO nikopa_dental_clinic.doctor(name, telephone, email) VALUES ('Chantel Burns', '+372 555 1208', 'chantel.burns@nikopa.ee');
+INSERT INTO nikopa_dental_clinic.doctor(name, telephone, email) VALUES ('Keeva Ryan', '+372 715 9091', 'keeva.ryan@nikopa.ee');
 
-INSERT INTO procedure(name) VALUES ('Konsultatsioon');
-INSERT INTO procedure(name) VALUES ('Hambaravi');
-INSERT INTO procedure(name) VALUES ('Hamba eemaldamine');
-INSERT INTO procedure(name) VALUES ('Proteseerimine');
-INSERT INTO procedure(name) VALUES ('Implantoloogia');
+INSERT INTO nikopa_dental_clinic.procedure(name, additional_fields)
+    VALUES ('Consultation', ('{"prices": [{"name": "Consultation (up to 15 minutes)", "value": "10€"}, ' ||
+     '{"name": "Consultation (up to 30 minutes)", "value": "20€"}, ' ||
+      '{"name": "Consultation for children", "value": "FREE"}, ' ||
+       '{"name": "Consultation for pensioners", "value": "FREE"}]}')::jsonb);
+INSERT INTO nikopa_dental_clinic.procedure(name, additional_fields)
+    VALUES ('Treatment', ('{"prices": [{"name": "Tooth filling", "value": "40-99€"}, ' ||
+     '{"name": "Temporary filling", "value": "20€"}, {"name": "Composite crown", "value": "120€"}, ' ||
+      '{"name": "Tooth filling for children", "value": "FREE"}]}')::jsonb);
+INSERT INTO nikopa_dental_clinic.procedure(name, additional_fields)
+    VALUES ('Tooth extraction', ('{"prices": [{"name": "Single-rooted tooth extraction", "value": "50€"}, ' ||
+     '{"name": "Multirooted tooth extraction", "value": "65€"}, ' ||
+      '{"name": "Surgical extraction of wisdom tooth", "value": "125€"}, ' ||
+       '{"name": "Tooth extraction for children", "value": "FREE"}]}')::jsonb);
+INSERT INTO nikopa_dental_clinic.procedure(name, additional_fields)
+    VALUES ('Prosthetics', ('{"prices": [{"name": "Ceramic crown", "value": "300€"}, ' ||
+     '{"name": "Zirconium crown", "value": "500€"}, {"name": "Impression", "value": "50€"}, ' ||
+      '{"name": "Temporary crown prepared during appointment", "value": "35€"}, ' ||
+       '{"name": "Temporary laboratory crown", "value": "160€"}]}')::jsonb);
+INSERT INTO nikopa_dental_clinic.procedure(name, additional_fields)
+    VALUES ('Implantation', ('{"prices": [{"name": "Placing an implant", "value": "600€"}, ' ||
+     '{"name": "Supra-construction", "value": "400€"}, {"name": "Crown on an implant", "value": "130€"}]}')::jsonb);
 
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (1, 1, '7:41',  '2019-07-30', '7:00 - 8:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (1, 1, '9:28',  '2019-07-30', '9:00 - 10:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (2, 1, '11:44', '2019-07-30', '11:00 - 12:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (2, 1, '18:40', '2019-07-30', '18:00 - 19:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (2, 1, '19:22', '2019-07-30', '19:00 - 20:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (2, 1, '18:36', '2019-07-30', '18:00 - 19:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (3, 2, '8:45',  '2019-07-30', '8:00 - 9:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (3, 2, '10:30', '2019-07-30', '10:00 - 11:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (4, 2, '11:35', '2019-07-30', '11:00 - 12:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (4, 3, '12:30', '2019-07-30', '12:00 - 13:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (4, 3, '14:15', '2019-07-30', '14:00 - 15:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (4, 3, '16:35', '2019-07-30', '16:00 - 17:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (5, 4, '8:45',  '2019-07-30', '8:00 - 9:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (5, 4, '11:15', '2019-07-30', '11:00 - 12:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (6, 4, '11:30', '2019-07-30', '11:00 - 11:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (7, 4, '16:35', '2019-07-30', '16:00 - 17:00');
-INSERT INTO visit(doctor_id, procedure_id, time, date, interval) VALUES (8, 5, '11:00', '2019-07-30', '11:00 - 12:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (1, 1, '7:41',  '2019-07-30', '7:00 - 8:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (1, 1, '9:28',  '2019-07-30', '9:00 - 10:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (2, 1, '11:44', '2019-07-30', '11:00 - 12:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (2, 1, '18:40', '2019-07-30', '18:00 - 19:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (2, 1, '19:22', '2019-07-30', '19:00 - 20:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (2, 1, '18:36', '2019-07-30', '18:00 - 19:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (3, 2, '8:45',  '2019-07-30', '8:00 - 9:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (3, 2, '10:30', '2019-07-30', '10:00 - 11:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (4, 2, '11:35', '2019-07-30', '11:00 - 12:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (4, 3, '12:30', '2019-07-30', '12:00 - 13:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (4, 3, '14:15', '2019-07-30', '14:00 - 15:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (4, 3, '16:35', '2019-07-30', '16:00 - 17:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (5, 4, '8:45',  '2019-07-30', '8:00 - 9:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (5, 4, '11:15', '2019-07-30', '11:00 - 12:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (6, 4, '11:30', '2019-07-30', '11:00 - 11:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (7, 4, '16:35', '2019-07-30', '16:00 - 17:00');
+INSERT INTO nikopa_dental_clinic.visit(doctor_id, procedure_id, time, date, interval)
+    VALUES (8, 5, '11:00', '2019-07-30', '11:00 - 12:00');
+
+INSERT INTO nikopa_dental_clinic.client(name, surname, telephone, email, visit_id, connection_type)
+    VALUES ('Frances', 'Slater', '+3728750917', 'frances@mail.eu', 1, 'PHONE');
+INSERT INTO nikopa_dental_clinic.client(name, surname, telephone, email, visit_id, connection_type)
+    VALUES ('Buddy', 'Schofield', '+3729098554', 'buddy@mail.eu', 2, 'EMAIL');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,24 @@
-CREATE TABLE doctor (
+DROP SCHEMA IF EXISTS nikopa_dental_clinic CASCADE;
+CREATE SCHEMA IF NOT EXISTS nikopa_dental_clinic;
+
+CREATE TABLE nikopa_dental_clinic.doctor (
   doctor_id SERIAL NOT NULL,
   name VARCHAR (255) NOT NULL,
+  telephone VARCHAR (15) NOT NULL,
+  email VARCHAR (255) NOT NULL,
+  photo_url VARCHAR (50),
+  additional_fields JSONB,
   CONSTRAINT pk_doctor_id PRIMARY KEY (doctor_id)
 );
 
-CREATE TABLE procedure (
+CREATE TABLE nikopa_dental_clinic.procedure (
   procedure_id SERIAL NOT NULL,
   name VARCHAR (255) NOT NULL,
+  additional_fields JSONB,
   CONSTRAINT pk_procedure_id PRIMARY KEY (procedure_id)
 );
 
-CREATE TABLE visit (
+CREATE TABLE nikopa_dental_clinic.visit (
   visit_id BIGSERIAL NOT NULL,
   doctor_id INT NOT NULL,
   procedure_id INT NOT NULL,
@@ -20,27 +28,27 @@ CREATE TABLE visit (
   is_active BOOLEAN NOT NULL DEFAULT TRUE,
   is_booked BOOLEAN NOT NULL DEFAULT FALSE,
   CONSTRAINT pk_visit_id PRIMARY KEY (visit_id),
-  CONSTRAINT fk_visit_doctor_id FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id)
+  CONSTRAINT fk_visit_doctor_id FOREIGN KEY (doctor_id) REFERENCES nikopa_dental_clinic.doctor(doctor_id)
     ON UPDATE CASCADE ON DELETE NO ACTION,
-  CONSTRAINT fk_visit_procedure_id FOREIGN KEY (procedure_id) REFERENCES procedure(procedure_id)
-    ON UPDATE CASCADE ON DELETE NO ACTION
+  CONSTRAINT fk_visit_procedure_id FOREIGN KEY (procedure_id) REFERENCES nikopa_dental_clinic.procedure(procedure_id)
+    ON UPDATE CASCADE ON DELETE NO ACTION,
+  CONSTRAINT chk_visit_interval_is_valid CHECK (visit.interval ~* '^[0-2]?\d[:]\d\d\s[-]\s[0-2]?\d[:]\d\d*$')
 );
+CREATE INDEX idx_visit_doctor_id ON nikopa_dental_clinic.visit(doctor_id);
+CREATE INDEX idx_visit_procedure_id ON nikopa_dental_clinic.visit(procedure_id);
 
-CREATE TABLE client (
+CREATE TABLE nikopa_dental_clinic.client (
   client_id BIGSERIAL NOT NULL,
   name VARCHAR (255) NOT NULL,
   surname VARCHAR (255) NOT NULL,
   telephone VARCHAR (60) NOT NULL,
   email VARCHAR (255) NOT NULL,
-  visit_id BIGINT NOT NULL,
+  visit_id BIGINT NOT NULL UNIQUE,
   connection_type VARCHAR(5) NOT NULL,
   CONSTRAINT pk_client_id PRIMARY KEY (client_id),
-  CONSTRAINT fk_client_visit_id FOREIGN KEY (visit_id) REFERENCES visit(visit_id)
+  CONSTRAINT fk_client_visit_id FOREIGN KEY (visit_id) REFERENCES nikopa_dental_clinic.visit(visit_id)
     ON UPDATE CASCADE ON DELETE NO ACTION,
   CONSTRAINT chk_client_telephone_is_valid CHECK (telephone ~* '^[+]*[(]?[+]?[0-9]*[)]?[-\s\d]*$'),
   CONSTRAINT chk_client_email_is_valid CHECK (email ~* '^([a-z0-9_\-\.]+)@([a-z0-9_\-\.]+)\.([a-z]{2,5})$')
 );
-
-CREATE INDEX idx_visit_doctor_id ON visit(doctor_id);
-CREATE INDEX idx_visit_procedure_id ON visit(procedure_id);
-CREATE INDEX idx_client_visit_id ON client(visit_id);
+CREATE INDEX idx_client_visit_id ON nikopa_dental_clinic.client(visit_id);
